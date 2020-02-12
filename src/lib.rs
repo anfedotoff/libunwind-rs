0dae4ad50a32b98e3f7b062eddb998af3536b57c
fedotoff <fedotoff@ispras.ru>
2020-02-12 17:13:18 +0300

    Added licence file

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@
 //! `libunwind-rs`  is a library providing safe Rust API for retrieving backtrace from local and
 //! remote process, and also from coredumps. Crate is build on a top of [libunwind] library.
 //! 
-//!
 //! [libunwind]: http://www.nongnu.org/libunwind/
 extern crate num_derive;
 
